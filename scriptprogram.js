fc51d98588df5268c02640f4c57b9912fd4d4524
Tomoow <lievenstom1997@gmail.com>
2023-09-05 14:43:14 +0200

    Finished first two pages, works

--- scriptprogram.js
+++ scriptprogram.js
@@ -1,20 +1,3 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-// Example of setting a cookie with SameSite=Lax
-document.cookie = "cookieName=cookieValue; SameSite=Lax";
-
-
-hamburger.addEventListener("click", () => {
-    console.log("Hamburger clicked");
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
-
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}))
-
 document.addEventListener("DOMContentLoaded", function () {
     // Get references to buttons and content sections
     const tabs = document.querySelectorAll(".tab-button");
@@ -38,13 +21,4 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById(contentId).classList.add("active");
       });
     });
-
-    // Make the first tab and its content section active on page load
-    tabs[0].classList.add("active");
-    const initialContentId = "content" + tabs[0].getAttribute("id").substr(3);
-    document.getElementById(initialContentId).classList.add("active");
-});
-
-// Wait for the DOM to fully load
-
-
+  });
